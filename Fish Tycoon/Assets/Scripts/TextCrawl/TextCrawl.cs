cd1ec87c343ff487bc912781bde2361a90f44ea6
charder <chardergaming@gmail.com>
2017-02-08 21:57:49 -0500

    I FOUND THE SOLUTION!
    Or at least a solution workable for this project. Wingdings B

--- Fish Tycoon/Assets/Scripts/TextCrawl/TextCrawl.cs
+++ Fish Tycoon/Assets/Scripts/TextCrawl/TextCrawl.cs
@@ -6,22 +6,35 @@ using UnityEngine.UI;
 public class TextCrawl : MonoBehaviour {
 	public float textSpeed;
 	public Text textRef;
-	public float textWidth;
+	float textWidth; 
+	int textSize; //current Text size, so if changed by best fit, it can re-size itself
 	Vector3 reset;
 	RectTransform canvasRect;
 	// Use this for initialization
 	void Start () {
+		textSize = 0;
+		textWidth = 0;
 		reset = transform.position;
 		textRef = GetComponent<Text> ();
-		textWidth = textRef.preferredWidth;
-		canvasRect = GameObject.Find ("MainCanvas").GetComponent<RectTransform> ();
-		
-		print(textRef.rectTransform.localPosition);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		print (canvasRect.rect.width);w
+		//resize textCrawl offset to compensate for resized screen (probably won't happen much, but for my sanity)
+		if (textRef.cachedTextGenerator.fontSizeUsedForBestFit != textSize) {
+			Font thisFont = textRef.font;
+			int newWidth = 0;
+			char[] textA = textRef.text.ToCharArray ();
+			CharacterInfo characterInfo = new CharacterInfo ();
+			foreach (char c in textA) {
+				thisFont.GetCharacterInfo (c, out characterInfo,textRef.cachedTextGenerator.fontSizeUsedForBestFit);
+				newWidth += characterInfo.advance;
+			}
+			textRef.rectTransform.offsetMax += new Vector2 (newWidth - textWidth,0);
+			textWidth = newWidth;
+			textSize = textRef.cachedTextGenerator.fontSizeUsedForBestFit;
+		}
+		//print (textRef.cachedTextGenerator.fontSizeUsedForBestFit);
 		if (Input.GetKeyDown (KeyCode.Alpha1)) {
 			textRef.transform.position = reset;
 		}
