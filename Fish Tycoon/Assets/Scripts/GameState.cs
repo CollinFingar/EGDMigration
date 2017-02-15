5e39e2c3d7659541cf2472de172c4b274b2718ec
charder <chardergaming@gmail.com>
2017-02-15 00:19:35 -0500

    GameState prefab can hold data that translates to the end game.
    You can relish in the simultaneous profits and deaths of many.

---
+++ Fish Tycoon/Assets/Scripts/GameState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameState : MonoBehaviour {
+	public int refsSaved;
+	public int refsDied;
+	public int funds;
+	public bool transitioned;
+	// Use this for initialization
+	void Start () {
+		transitioned = false;
+		DontDestroyOnLoad (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnLevelWasLoaded() {
+		if (transitioned) {
+			Destroy (this.gameObject);
+		} else {
+			transitioned = true;
+			SetEndText ();
+		}
+	}
+
+	void SetEndText() {
+		GameObject.Find ("RefsSavedText").GetComponent<Text> ().text += refsSaved;
+		GameObject.Find ("RefsDiedText").GetComponent<Text> ().text += refsDied;
+		GameObject.Find ("FundsText").GetComponent<Text> ().text += funds;
+	}
+}
