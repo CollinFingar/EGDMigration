a56e77f5943b8781b153c9b130263bdaccb3707b
charder <chardergaming@gmail.com>
2017-02-14 23:27:54 -0500

    Some more work on scenes.
    If you build it... scenes will change.

---
+++ Fish Tycoon/Assets/Scripts/TrackGameState.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrackGameState : MonoBehaviour {
+	public int refSaved;
+	public int refDied;
+	public int funds;
+
+	public string gameScene;
+	public string endScene;
+	public string menuScene;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void StartGame () {
+		SceneManager.LoadScene (gameScene);
+	}
+
+	public void EndGame () {
+		SceneManager.LoadScene (endScene);
+	}
+
+	public void RestartGame() {
+		SceneManager.LoadScene (menuScene);
+	}
+		
+
+
+}
