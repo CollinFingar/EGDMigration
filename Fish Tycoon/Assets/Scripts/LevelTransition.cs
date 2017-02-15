b758131a162f1133436c5d6406743cfe8ed278f7
charder <chardergaming@gmail.com>
2017-02-14 23:41:51 -0500

    Level Transitions, just as Level Transition.prefab, he's smart.
    You know, for kids...

--- Fish Tycoon/Assets/Scripts/LevelTransition.cs
+++ Fish Tycoon/Assets/Scripts/LevelTransition.cs
@@ -3,22 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TrackGameState : MonoBehaviour {
-	public int refSaved;
-	public int refDied;
-	public int funds;
-
+public class LevelTransition : MonoBehaviour {
 	public string gameScene;
 	public string endScene;
 	public string menuScene;
 
 	// Use this for initialization
 	void Start () {
-		DontDestroyOnLoad (gameObject);
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 	}
 
 	public void StartGame () {
